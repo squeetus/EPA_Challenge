b9c297bf2ec77677edf642d14a538e243a6883ee
squeetus <burlinsond2@gmail.com>
2016-04-24 17:54:29 -0400

    cleaned up server.js file

--- server.js
+++ server.js
@@ -3,54 +3,27 @@ var express = require('express'),
     mongoose = require('mongoose'),
     config = require('./config/config');
 
-console.log(config);
-
-// load in mongodb models
+/* Set up Mongo connection and models */
 var modelsPath = __dirname + '/app/models';
 require(modelsPath + '/chemicals.js');
 require(modelsPath + '/facilities.js');
-
-var EPAFileParser = require('./app/controllers/EPAFileParser');
-
-mongoose.connect('mongodb://localhost/EPA_University_Challenge', function(err){
+mongoose.connect(config.db, function(err){
  if(err) console.log("Mongoose error: ", err);
 });
 
 
+var EPAFileParser = require('./app/controllers/EPAFileParser');
 // EPAFileParser.testChemical();
 // EPAFileParser.testFacility();
 // EPAFileParser.clearDBS();
-
 // run with --max-old-space-size=4096 to allocate enough memory
 // EPAFileParser.readFiles();
 
-
-
-
-
-app.set('views', './app/views');
-app.set('view engine', 'jade');
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.listen(config.port, function () {
+  console.log('EPA Challenge app listening on port', config.port, '!');
 });
 
-app.get('/home', function( req, res ) {
+/* Load Routes */
+require('./config/routes/index')( app );
 
-  res.render('index', {tits: "lol"});
-});
-
-app.get('/readFile', function( req, res ) {
-  EPAFileParser.read(1987); // default file when no arg is passed
-  res.render('index', {file: ["1987"]});
-});
-
-app.get('/readFile/:number', function( req, res ) {
-  console.log(req.params.number);
-  EPAFileParser.read(1);
-  res.sendStatus(200);
-});
-
-// app.listen(4040, function () {
-//   console.log('EPA Challenge app listening on port 4040!');
-// });
+exports = module.exports = app;
