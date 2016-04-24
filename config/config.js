57978825e41fcd46e18a7ca43a40ea44a91490c9
squeetus <burlinsond2@gmail.com>
2016-04-24 17:55:30 -0400

    set up development credentials in config file

--- config/config.js
+++ config/config.js
@@ -8,11 +8,9 @@ var config = {
     app: {
       name: 'bridgesapi'
     },
-    port: 3000,
-    // db: 'mongodb://heroku_app27208241:lg0jm38s5r1pbl0g6e68fcbiih@ds061228.mongolab.com:61228/heroku_app27208241'
-    db: 'mongodb://localhost/bridgesapi-development'
+    port: 4040,
+    db: 'mongodb://localhost/EPA_University_Challenge'
   }
 };
 
-console.log(config);
-module.exports = config[0];
+module.exports = config.development;
